d6b78cbe9e0f62485dea546c3a26f14d731fcccd
Matt Casanova <lazersquad@gmail.com>
2019-04-28 16:40:42 -0700

    Add Bottom Nav and icons

--- app/src/main/java/com/mattcasanova/backstackdemo/MainActivity.kt
+++ app/src/main/java/com/mattcasanova/backstackdemo/MainActivity.kt
@@ -2,11 +2,24 @@ package com.mattcasanova.backstackdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        supportActionBar?.title = "Hello World"
+
     }
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        return true
+    }
+
 }
